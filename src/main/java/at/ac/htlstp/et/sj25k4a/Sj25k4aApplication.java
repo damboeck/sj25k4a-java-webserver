9fc9386172f0d299df608fad0cbb5d6418d77f45
Werner Damboeck <werner.damboeck@htlstp.ac.at>
2026-01-07 15:51:52 +0100

    Initial commit

---
+++ src/main/java/at/ac/htlstp/et/sj25k4a/Sj25k4aApplication.java
@@ -0,0 +1,13 @@
+package at.ac.htlstp.et.sj25k4a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sj25k4aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sj25k4aApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/ac/htlstp/et/sj25k4a/Sj25k4aApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.htlstp.et.sj25k4a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sj25k4aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
